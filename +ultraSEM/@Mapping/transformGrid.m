8cf69c37f6d6a304929371fddc3583cb9da25212
ilic-stefan <ilicstef@grinnell.edu>
2020-07-27 16:30:58 -0500

    update documentation for transformGrid

--- +ultraSEM/@Mapping/transformGrid.m
+++ +ultraSEM/@Mapping/transformGrid.m
@@ -1,5 +1,11 @@
 function [X, Y, XY] = transformGrid(T, x, y)
-%TRANSFORMGRID   Map a grid.
+%TRANSFORMGRID   Map a point (x,y) using mapping T.
+%
+% Commonly used to map a point (x,y) from [-1 1 -1 1] square to Quad domain
+% specified by T.
+%
+% X is the new coordinate of x, Y is the new coordinate of y, and
+% XY = [X, Y].
 
 %   Copyright 2020 Dan Fortunato, Nick Hale, and Alex Townsend.
 
